94de2999d3539e77cbfc9cd0188c334c8b095863
elydoodson <elydoodson@hotmail.co.uk>
2019-09-12 12:42:16 -0400

    Initialisation of group

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-#%%
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from os import listdir
 from scipy.stats import linregress
-
-#%%
 
 
 class Star:
@@ -21,4 +18,22 @@ def calculate_line(m, x, c):
     return np.add(np.dot(m, x), c)
 
 
-#%%
+def get_data(location):
+    """
+    Takes string as input and returns a list of Star objects
+    """
+    data_frame = pd.read_csv(location, encoding="utf-8", delimiter="\t", comment="#")
+
+    mass = data_frame.M.tolist()
+    period = data_frame.Per.tolist()
+
+    return [Star(period, mass) for mass, period in zip(mass, period)]
+
+
+path = "d:data\Pleiades_Hartman.csv"
+# path = "/home/edoodson/Documents/spin_down/data/Pleiades_Hartman.csv"
+
+
+star_list = get_data(path)
+for star in star_list:
+    star.set_initial_group()
