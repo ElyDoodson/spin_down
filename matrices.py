f24697b1960d3bd02fa084e5d8d83d9d3bf11228
elydoodson <elydoodson@hotmail.co.uk>
2019-09-18 16:52:18 -0400

    Weight caclculating functionality

--- matrices.py
+++ matrices.py
@@ -39,6 +39,34 @@ def set_initial_star_group(star_list, coefficients=[7, -5, 0]):
         )
 
 
+def set_star_group(star_list, slow_coefficients, fast_coefficients):
+    """
+    Sets the group of the stars in star_list to the closest line
+
+    Parameters
+    -------
+        star_list: list
+            list of all star objects
+        slow_coefficients: list
+            list of [b0,b1,b2] for slow line
+        fast_coefficients: list
+            lsit of [b0,0,0] for fast line (flat line so only intercept)
+    
+    Returns
+    ------
+        None
+
+    """
+    for star in star_list:
+        dist_slow = (
+            predict_value(star.predictors, slow_coefficients) - star.period
+        ) ** 2
+        dist_fast = (
+            predict_value(star.predictors, fast_coefficients) - star.period
+        ) ** 2
+        star.group = 1 if min(dist_fast, dist_slow) == dist_slow else 0
+
+
 def predict_value(star_predictors, line_data):
     """
     Parameters
@@ -91,14 +119,62 @@ def calculate_coefficients(
     lr.coef_ = lr.coef_ if set_slope == False else np.zeros(len(lr.coef_))
 
     if return_linregg == True:
-        return lr.intercept_ + lr.coef_[1:], lr
+        return np.append(lr.intercept_, lr.coef_[1:]), lr
 
     elif return_linregg == False:
-        return lr.intercept_ + lr.coef_[1:]
+        return np.append(lr.intercept_, lr.coef_[1:])
 
     else:
         print("return_linregg must be a boolean")
         return None
+
+
+# def calculate_ser(predict_y1, predict_y0, y):
+#     return (predict_y1 - y) ** 2 / (predict_y0 - y) ** 2
+
+
+def calculate_weight(star_list, coef_slow, coef_fast, selected_group, selected_weight):
+    """
+    Takes the star list and calculates the weight of the stars w.r.t 
+    the chosen line
+
+    Parameters
+    -------
+        star_list: list
+            List of Star objects
+        coef_slow: list
+            list of slow coefficients
+        coef_fast: list
+            list of fast coefficients
+
+        selected_group: int
+            Selected group of stars you wish to work with. 1 or 0 (slow or fast)
+        selected_weight: str, "slow" or "fast"
+            Selected weight to return
+    
+    Returns
+    -------
+        list: list of weights of the selected star group w.r.t the selected line
+    """
+    # Calculating recuring values
+    star_periods = np.array(
+        [star.period for star in star_list if star.group == selected_group]
+    )
+    star_predictors = [
+        star.predictors for star in star_list if star.group == selected_group
+    ]
+    # Calculating square of the residuals
+    square_res = np.divide(
+        np.square(predict_value(star_predictors, coef_slow) - star_periods),
+        np.square(predict_value(star_predictors, coef_fast) - star_periods),
+    )
+
+    weight_slow = np.divide(1.0, (1 + square_res))
+    # weight_fast = np.divide(1., (1 + np.divide(1,square_res)))
+    if selected_weight == "slow":
+        return weight_slow
+    elif selected_weight == "fast":
+        return 1 - weight_slow
 
 
 #%% DATA INITIALISATION
@@ -111,15 +187,16 @@ set_initial_star_group(star_list)
 
 #%%
 coefficients_slow, lrs = calculate_coefficients(
-    [star for star in star_list], group=0, return_linregg=True, set_slope=True
-)
-coefficients_fast, lrf = calculate_coefficients(
     [star for star in star_list], group=1, return_linregg=True
 )
+coefficients_fast, lrf = calculate_coefficients(
+    [star for star in star_list], group=0, return_linregg=True, set_slope=True
+)
 
 
-fig, ax = plt.subplots(1)
+fig, ax = plt.subplots(1, figsize=(8, 6))
 ax.invert_xaxis()
+ax.set(title="GROUP SPLIT BY LINE 5X + 7")
 ax.scatter(
     [star.mass for star in star_list if star.group == 1],
     [star.period for star in star_list if star.group == 1],
@@ -132,16 +209,167 @@ ax.scatter(
     marker="x",
     color="red",
 )
-ax.scatter(
-    [star.mass for star in star_list],
-    lrs.predict([star.predictors for star in star_list]),
+ax.plot(
+    [i for i in np.arange(1.4, 0.2, -0.01)],
+    lrs.predict([[1, i, i ** 2] for i in np.arange(1.4, 0.2, -0.01)]),
+    color="blue",
+)
+ax.plot(
+    [i for i in np.arange(1.4, 0.2, -0.01)],
+    lrf.predict([[1, i, i ** 2] for i in np.arange(1.4, 0.2, -0.01)]),
     color="red",
 )
+
+
+#%% SET GROUPS TO CLOSEST LINE
+coefficients_slow, lrs = calculate_coefficients(
+    [star for star in star_list], group=1, return_linregg=True
+)
+coefficients_fast, lrf = calculate_coefficients(
+    [star for star in star_list], group=0, return_linregg=True, set_slope=True
+)
+
+set_star_group(star_list, coefficients_slow, coefficients_fast)
+
+coefficients_slow, lrs = calculate_coefficients(
+    [star for star in star_list], group=1, return_linregg=True
+)
+coefficients_fast, lrf = calculate_coefficients(
+    [star for star in star_list], group=0, return_linregg=True, set_slope=True
+)
+
+fig, ax = plt.subplots(1, figsize=(8, 6))
+ax.invert_xaxis()
+ax.set(title="GROUPS TO CLOSEST LINE")
 ax.scatter(
-    [star.mass for star in star_list],
-    lrf.predict([star.predictors for star in star_list]),
+    [star.mass for star in star_list if star.group == 1],
+    [star.period for star in star_list if star.group == 1],
+    marker="x",
+    color="blue",
+)
+ax.scatter(
+    [star.mass for star in star_list if star.group == 0],
+    [star.period for star in star_list if star.group == 0],
+    marker="x",
+    color="red",
+)
+ax.plot(
+    [i for i in np.arange(1.4, 0.2, -0.01)],
+    lrs.predict([[1, i, i ** 2] for i in np.arange(1.4, 0.2, -0.01)]),
+    color="blue",
+)
+ax.plot(
+    [i for i in np.arange(1.4, 0.2, -0.01)],
+    lrf.predict([[1, i, i ** 2] for i in np.arange(1.4, 0.2, -0.01)]),
+    color="red",
+)
+print("Slow coeff =", coefficients_slow)
+print("Fast coeff =", coefficients_fast)
+
+
+#%%
+# set_star_group(star_list, coefficients_slow, coefficients_fast)
+sample_weight_slow = calculate_weight(
+    star_list, coefficients_slow, coefficients_fast, 1, "slow"
+)
+sample_weight_fast = calculate_weight(
+    star_list, coefficients_slow, coefficients_fast, 0, "fast"
+)
+
+coefficients_slow, lrs = calculate_coefficients(
+    [star for star in star_list],
+    group=1,
+    return_linregg=True,
+    sample_weights=sample_weight_slow,
+)
+coefficients_fast, lrf = calculate_coefficients(
+    [star for star in star_list],
+    group=0,
+    return_linregg=True,
+    set_slope=True,
+    sample_weights=sample_weight_fast,
+)
+
+fig, ax = plt.subplots(2, figsize=(8, 6))
+ax[0].invert_xaxis()
+ax[1].invert_xaxis()
+ax[0].set(title="NaN")
+ax[0].scatter(
+    [star.mass for star in star_list if star.group == 1],
+    [star.period for star in star_list if star.group == 1],
+    marker="x",
+    color="blue",
+)
+ax[0].scatter(
+    [star.mass for star in star_list if star.group == 0],
+    [star.period for star in star_list if star.group == 0],
+    marker="x",
+    color="red",
+)
+ax[0].plot(
+    [i for i in np.arange(1.4, 0.2, -0.01)],
+    lrs.predict([[1, i, i ** 2] for i in np.arange(1.4, 0.2, -0.01)]),
+    color="blue",
+)
+ax[0].plot(
+    [i for i in np.arange(1.4, 0.2, -0.01)],
+    lrf.predict([[1, i, i ** 2] for i in np.arange(1.4, 0.2, -0.01)]),
+    color="red",
+)
+
+ax[1].scatter(
+    [star.mass for star in star_list if star.group == 1],
+    [star.period for star in star_list if star.group == 1],
+    marker="x",
+    color="blue",
+)
+ax[1].scatter(
+    [star.mass for star in star_list if star.group == 0],
+    [star.period for star in star_list if star.group == 0],
+    marker="x",
+    color="red",
+)
+ax[1].plot(
+    [i for i in np.arange(1.4, 0.2, -0.01)],
+    predict_value(
+        [[1, i, i ** 2] for i in np.arange(1.4, 0.2, -0.01)], coefficients_slow
+    ),
     color="blue",
 )
 
 
+print("Slow coeff =", coefficients_slow)
+print("Fast coeff =", coefficients_fast)
+
+
 #%%
+set_star_group(star_list, coefficients_slow, coefficients_fast)
+
+
+fig, ax = plt.subplots(1, figsize=(8, 6))
+ax.invert_xaxis()
+ax.set(title="NaN")
+ax.scatter(
+    [star.mass for star in star_list if star.group == 1],
+    [star.period for star in star_list if star.group == 1],
+    marker="x",
+    color="blue",
+)
+ax.scatter(
+    [star.mass for star in star_list if star.group == 0],
+    [star.period for star in star_list if star.group == 0],
+    marker="x",
+    color="red",
+)
+ax.plot(
+    [i for i in np.arange(1.4, 0.2, -0.01)],
+    lrs.predict([[1, i, i ** 2] for i in np.arange(1.4, 0.2, -0.01)]),
+    color="blue",
+)
+ax.plot(
+    [i for i in np.arange(1.4, 0.2, -0.01)],
+    lrf.predict([[1, i, i ** 2] for i in np.arange(1.4, 0.2, -0.01)]),
+    color="red",
+)
+print("Slow coeff =", coefficients_slow)
+print("Fast coeff =", coefficients_fast)
