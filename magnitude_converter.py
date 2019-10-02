2ef2063fad79b73781077e73083372a2469bf7fc
elydoodson <elydoodson@hotmail.co.uk>
2019-10-02 13:25:10 -0400

    file/dataframe dictionary init

--- magnitude_converter.py
+++ magnitude_converter.py
@@ -2,62 +2,41 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import os
 
 #%% FUNCTIONS
 
 
-def abs_from_app(vmag, distance_parsec):
-    return vmag - (5 * np.log10(distance_parsec)) + 5
-
-
-def vmag_to_mass(vmag):
-    c0 = 0.19226
-    c1 = -0.050737
-    c2 = 0.010137
-    c3 = -0.00075399
-    c4 = -1.9858e-05
-    x0 = 13.0
-
-    first = c1 * (vmag - x0)
-    second = c2 * (vmag - x0) ** 2
-    third = c3 * (vmag - x0) ** 3
-    forth = c4 * (vmag - x0) ** 4
-    return c0 + first + second + third + forth
+def app_to_abs(app_mag, distance_parsec):
+    return app_mag - (5 * np.log10(distance_parsec)) + 5
 
 
 #%%
-path = "D:\dev\spin_down\OCdata/"
-source = "M37PC.tsv"
-dist = 1383
-age = 550
+
+path = "D:/dev/spin_down/new_data/"
+
+cluster_list = os.listdir(path)
+files = [os.listdir(path + str(cluster)) for cluster in cluster_list]
 
 
-df = pd.read_csv(path + source, sep="\t", comment="#")
+# dict_list = [{"name": cluster, "data_frame": np.concatenate([pd.read_csv(path + file) for file in files])} for index, cluster in enumerate(cluster_list)]
 
+[file for file in files]
 
-periods = df.Prot.to_numpy()
-vmag = df.Vmag.to_numpy()
+dict_list = [
+    {
+        "name": cluster_list[i],
+        "data_frames": [
+            pd.read_csv(
+                filepath_or_buffer=path + cluster_list[i] + "/" + file,
+                comment="#",
+                delimiter="\t",
+                skipinitialspace=True,
+            )
+            for file in file_list
+        ],
+    }
+    for i, file_list in enumerate(files)
+]
 
-
-print(df.tail())
-print(df.Vmag.describe())
-
-abs_vmag = abs_from_app(vmag, dist)
-
-masses = vmag_to_mass(abs_vmag)
-
-converted_df = pd.DataFrame({"Per": periods, "M": masses})
-
-
-converted_df.to_csv(
-    path + "p_m/" + source[:3] + "pm_" + str(age) + ".csv",
-    sep="\t",
-    index=None,
-    header=True,
-)
-#%%
-
-fig, ax = plt.subplots(1, figsize=(10, 6))
-ax.invert_xaxis()
-ax.set(ylim=(-2, 30), title=source[:-4])
-ax.scatter(masses, periods)
+[datum["data_frames"] for datum in dict_list]
