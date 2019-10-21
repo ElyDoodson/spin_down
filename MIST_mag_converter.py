868a99dc4a8e1c8b628c8ae88b563ad936263b53
elydoodson <elydoodson@hotmail.co.uk>
2019-10-21 18:03:38 -0400

    MIST Mag to mass

--- MIST_mag_converter.py
+++ MIST_mag_converter.py
@@ -8,6 +8,15 @@ from sklearn.preprocessing import PolynomialFeatures
 from astropy.table import Table
 from astropy.io import fits
 
+
+def app_to_abs(app_mag, distance_parsec):
+    return app_mag - (5 * np.log10(distance_parsec)) + 5
+
+
+def abs_to_app(abs_mag, distance_parsec):
+    return abs_mag - (5 * np.log10(distance_parsec)) + 5
+
+
 #%%
 
 filename1 = "new_MIST_data/MIST_V1.2_feh0_afe0.fits"
@@ -21,7 +30,7 @@ filename2 = "new_MIST_data/MIST_V1.2_feh0_afe0_wPHOT.fits"
 data2 = Table.read(filename2, format="fits")
 photometry = data2.to_pandas()
 # photometry.describe()
-# list(df2.columns.values)
+# list(photometry.columns.values)
 
 #%%
 photometry = photometry[features["star_mass"] <= 1.4]
@@ -29,14 +38,14 @@ features = features[features["star_mass"] <= 1.4]
 # features.describe()
 
 #%%
-age = 500 * 10 ** 6
-# features.iloc[(features['star_age']/10**6 - age).abs().argsort()[:2]]
-# features.loc[abs(features['star_age'] - age) <= 10000000]
+# age = 78 * 10 ** 6
+# # features.iloc[(features['star_age']/10**6 - age).abs().argsort()[:2]]
+# # features.loc[abs(features['star_age'] - age) <= 10000000]
 
 
-fig, ax = plt.subplots(1, figsize=(11.5, 7))
+# fig, ax = plt.subplots(1, figsize=(11.5, 7))
 
-ax.plot(features["star_mass"])
+# ax.plot(features["star_mass"])
 
 
 #%%
@@ -49,6 +58,8 @@ for mass in features["star_mass"]:
     initial_mass = mass
 
 #%%
+age = 130 * 10 ** 6
+
 bool_list = [
     [
         True if abs(star_mass - mass_ele) <= mass_difference_threshold else False
@@ -57,16 +68,45 @@ bool_list = [
     for mass_ele in mass_list
 ]
 
-photometry_list = [
-    photometry.iloc[
-        features[booleans]
-        .index[(features["star_age"] / 10 ** 6 - age).abs().argsort()[:2]]
-        .to_list()
+photometry_df = pd.concat(
+    [
+        photometry.iloc[
+            features[booleans]
+            .index[(features[booleans]["star_age"]  - age).abs().argsort()[:2]]
+            .to_list()
+        ]
+        for booleans in bool_list
     ]
-    for booleans in bool_list
-]
+)
 
-kmag_list = np.array([ pair["2MASS_Ks"].to_list() for pair in photometry_list])
+# kmag_list = np.array([pair["2MASS_Ks"].to_list() for pair in photometry_list])
 
-star_mag = 5.6
-min(np.concatenate(kmag_list), key=lambda x:abs(x-star_mag))
+# star_mag = 5.6
+# min(np.concatenate(kmag_list), key=lambda x: abs(x - star_mag))
+
+#%%
+file_path = "D:/dev/spin_down/new_data/m50/irwin_2009.tsv"
+cluster = pd.read_csv(file_path, comment="#", delimiter="\t", skipinitialspace=True)
+
+#%%
+distance = 881
+star_mag = app_to_abs(15.911, distance)
+
+chosen_mass_index = photometry_df.iloc[
+    (photometry_df["Bessell_I"] - star_mag).abs().argsort()[:1]
+].index[0]
+
+# features.iloc[chosen_mass_index].star_mass
+fig, ax = plt.subplots(1, figsize=(11.5, 7))
+
+photometry_df.describe()
+plt.scatter(
+    photometry_df["Bessell_I"],
+    features.star_mass[photometry_df["Bessell_I"].index.to_list()],
+)
+plt.scatter(app_to_abs(cluster.Icmag.to_numpy(),distance), cluster.Mass.to_list())
+
+#%%
+
+
+#%%
