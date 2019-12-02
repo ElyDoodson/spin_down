bad4eab828a94ecb310c7b181c870bc524a8d71c
elydoodson <elydoodson@hotmail.co.uk>
2019-12-02 16:09:26 -0500

    added Tau to data files

--- mist_converter.py
+++ mist_converter.py
@@ -9,6 +9,7 @@ from astropy.table import Table
 from astropy.io import fits
 import matplotlib.gridspec as gridspec
 
+plt.style.use("dark_background")
 #%% FUNCTIONS BEING DECLARED
 def app_to_abs(app_mag, distance_parsec, extinction=0):
     return app_mag - (5 * np.log10(distance_parsec)) + 5 - extinction
@@ -18,11 +19,14 @@ def abs_to_app(abs_mag, distance_parsec, extinction=0):
     return abs_mag - (5 * np.log10(distance_parsec)) + 5 - extinction
 
 
-def one_above_below(df, mag_name, mag_value):
+def one_above_below(df, value_name, value):
+    """
+    Returns a dataframe with value above and below the specified value 
+    """
     return pd.concat(
         [
-            df[df[mag_name] == df[df[mag_name] > mag_value][mag_name].min()],
-            df[df[mag_name] == df[df[mag_name] < mag_value][mag_name].max()],
+            df[df[value_name] == df[df[value_name] > value][value_name].min()],
+            df[df[value_name] == df[df[value_name] < value][value_name].max()],
         ]
     )
 
@@ -32,7 +36,6 @@ def mist_mass_interpolate(photom_dataframe, feature_df, abs_mags, mag_str):
     for abs_mag in abs_mags:
         # Choses a value in photom_dataframe[mag_str] above and below the value of abs_mag
         x = one_above_below(photom_dataframe, mag_str, abs_mag)[mag_str].to_numpy()
-        # display(one_above_below(photom_dataframe, mag_str, abs_mag))
         # Returns the index of the sorted "above and below" values
         # (this is required for np.interp to work)
         index = x.argsort()
@@ -46,11 +49,38 @@ def mist_mass_interpolate(photom_dataframe, feature_df, abs_mags, mag_str):
         lst.append(np.interp(abs_mag, x, y))
     return np.array(lst)
 
+def mist_tau_interpolate(data_frame, mass_list, age, age_err):
+    lst = []
+    age_reduced_df = data_frame.loc[
+        (data_frame.star_age >= age - age_err) & (data_frame.star_age <= age + age_err)
+    ]
+    for mass in mass_list:
+        # Creates DF from age_reduced df, contraining the mass value 1 above and below chosen value
+        oneup_onedown = one_above_below(age_reduced_df, "star_mass", mass)
+        x = oneup_onedown["star_mass"].to_numpy()
+        
+        # Returns the index of the sorted "above and below" values
+        # (this is required for np.interp to work)
+        index = x.argsort()
+        
+        # The two are sorted ascending
+        x = x[index]
+
+        # The coresponding tau values for the two masses is found and used in the linear interpolation
+        y = data_frame.iloc[
+            oneup_onedown.index
+        ].conv_env_turnover_time_g.to_numpy()[index]
+
+        lst.append(np.interp(mass, x, y))
+    return np.array(lst)
+
 
 #%% READING AND ASSIGNING FIT FILES
 filename1 = "new_MIST_data/MIST_V1.2_feh0_afe0.fits"
 data1 = Table.read(filename1, format="fits")
 features = data1.to_pandas()
+features_with_tau = pd.read_csv("D:/dev/spin_down/new_MIST_data/tau_mist.csv", sep="\t")
+# features_with_tau = features_with_tau.sort_values(by = "star_mass")
 # features.describe()
 # list(features.columns.values)
 
@@ -96,14 +126,15 @@ abs_mags = app_to_abs(data.Vmag.to_numpy(), dist, reddening)
 
 period = data.Prot.to_numpy()
 mass = mist_mass_interpolate(df, features, abs_mags, mag_str)
+tau = mist_tau_interpolate(features_with_tau, mass, age, age_err)
 
 fig, ax = plt.subplots(1, figsize=(11.5, 7))
 ax.invert_xaxis()
 ax.set(title=name, xlabel="Mass (M_Solar)", ylabel="Period (days)")
 ax.scatter(mass, period, color="green")
 
-data_dict = {"Per": period, "Mass": mass}
-data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass"])
+data_dict = {"Per": period, "Mass": mass, "Tau": tau}
+data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass", "Tau"])
 current_dict = {"age": age, "data_frame": data_frame}
 cluster_dict.update({name: current_dict})
 #%% Hyades
@@ -129,13 +160,15 @@ data = pd.read_csv(path, comment="#", delimiter="\t", skipinitialspace=True)
 # mass = mist_mass_interpolate(df, features, abs_mags, mag_str)
 
 mass = data.Mass.to_numpy()
+tau = mist_tau_interpolate(features_with_tau, mass, age, age_err)
+
 fig, ax = plt.subplots(1, figsize=(11.5, 7))
 ax.invert_xaxis()
 ax.set(title=name, xlabel="Mass (M_Solar)", ylabel="Period (days)")
 ax.scatter(mass, data.K2Per.to_numpy(), color="green")
 
-data_dict = {"Per": data.K2Per.to_numpy(), "Mass": mass}
-data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass"])
+data_dict = {"Per": data.K2Per.to_numpy(), "Mass": mass, "Tau": tau}
+data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass", "Tau"])
 current_dict = {"age": age, "data_frame": data_frame}
 cluster_dict.update({name: current_dict})
 
@@ -165,14 +198,15 @@ abs_mags = app_to_abs(data.Kmag.to_numpy(), dist, reddening)
 period = data.Per.to_numpy()
 mass = mist_mass_interpolate(df, features, abs_mags, mag_str)
 # mass = data.Mass.to_numpy()
+tau = mist_tau_interpolate(features_with_tau, mass, age, age_err)
 
 fig, ax = plt.subplots(1, figsize=(11.5, 7))
 ax.invert_xaxis()
 ax.set(title=name, xlabel="Mass (M_Solar)", ylabel="Period (days)")
 ax.scatter(mass, period, color="green")
 
-data_dict = {"Per": period, "Mass": mass}
-data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass"])
+data_dict = {"Per": period, "Mass": mass, "Tau": tau}
+data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass", "Tau"])
 current_dict = {"age": age, "data_frame": data_frame}
 cluster_dict.update({name: current_dict})
 #%% M34
@@ -200,14 +234,15 @@ abs_mags = app_to_abs(data.V0mag.to_numpy(), dist, reddening)
 
 period = data.Prot.to_numpy()
 mass = mist_mass_interpolate(df, features, abs_mags, mag_str)
+tau = mist_tau_interpolate(features_with_tau, mass, age, age_err)
 
 fig, ax = plt.subplots(1, figsize=(11.5, 7))
 ax.invert_xaxis()
 ax.set(title=name, xlabel="Mass (M_Solar)", ylabel="Period (days)")
 ax.scatter(mass, period, color="green")
 
-data_dict = {"Per": period, "Mass": mass}
-data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass"])
+data_dict = {"Per": period, "Mass": mass, "Tau": tau}
+data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass", "Tau"])
 current_dict = {"age": age, "data_frame": data_frame}
 cluster_dict.update({name: current_dict})
 
@@ -237,14 +272,15 @@ abs_mags = app_to_abs(data.V0mag.to_numpy(), dist, reddening)
 
 period = data.Prot.to_numpy()
 mass = mist_mass_interpolate(df, features, abs_mags, mag_str)
+tau = mist_tau_interpolate(features_with_tau, mass, age, age_err)
 
 fig, ax = plt.subplots(1, figsize=(11.5, 7))
 ax.invert_xaxis()
 ax.set(title=name, xlabel="Mass (M_Solar)", ylabel="Period (days)")
 ax.scatter(mass, period, color="green")
 
-data_dict = {"Per": period, "Mass": mass}
-data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass"])
+data_dict = {"Per": period, "Mass": mass, "Tau": tau}
+data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass", "Tau"])
 current_dict = {"age": age, "data_frame": data_frame}
 cluster_dict.update({name: current_dict})
 
@@ -277,14 +313,15 @@ abs_mags = app_to_abs(data.Vmag.to_numpy(), dist, reddening)
 
 period = data.Per.to_numpy()
 mass = mist_mass_interpolate(df, features, abs_mags, mag_str)
+tau = mist_tau_interpolate(features_with_tau, mass, age, age_err)
 
 fig, ax = plt.subplots(1, figsize=(11.5, 7))
 ax.invert_xaxis()
 ax.set(title=name, xlabel="Mass (M_Solar)", ylabel="Period (days)")
 ax.scatter(mass, period, color="green")
 
-data_dict = {"Per": period, "Mass": mass}
-data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass"])
+data_dict = {"Per": period, "Mass": mass, "Tau": tau}
+data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass", "Tau"])
 current_dict = {"age": age, "data_frame": data_frame}
 cluster_dict.update({name: current_dict})
 
@@ -315,14 +352,15 @@ abs_mags = app_to_abs(data.Icmag.to_numpy(), dist, reddening)
 period = data.Per.to_numpy()
 mass = mist_mass_interpolate(df, features, abs_mags, mag_str)
 # mass = data.Mass.to_numpy()
+tau = mist_tau_interpolate(features_with_tau, mass, age, age_err)
 
 fig, ax = plt.subplots(1, figsize=(11.5, 7))
 ax.invert_xaxis()
 ax.set(title=name, xlabel="Mass (M_Solar)", ylabel="Period (days)")
 ax.scatter(mass, period, color="green")
 
-data_dict = {"Per": period, "Mass": mass}
-data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass"])
+data_dict = {"Per": period, "Mass": mass, "Tau": tau}
+data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass", "Tau"])
 current_dict = {"age": age, "data_frame": data_frame}
 cluster_dict.update({name: current_dict})
 
@@ -351,14 +389,15 @@ abs_mags = app_to_abs(data.Rmag.to_numpy(), dist, reddening)
 
 period = data.Per.to_numpy()
 mass = mist_mass_interpolate(df, features, abs_mags, mag_str)
+tau = mist_tau_interpolate(features_with_tau, mass, age, age_err)
 
 fig, ax = plt.subplots(1, figsize=(11.5, 7))
 ax.invert_xaxis()
 ax.set(title=name, xlabel="Mass (M_Solar)", ylabel="Period (days)")
 ax.scatter(mass, period, color="green")
 
-data_dict = {"Per": period, "Mass": mass}
-data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass"])
+data_dict = {"Per": period, "Mass": mass, "Tau": tau}
+data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass", "Tau"])
 current_dict = {"age": age, "data_frame": data_frame}
 cluster_dict.update({name: current_dict})
 
@@ -388,14 +427,15 @@ abs_mags = app_to_abs(data.Vmag.to_numpy(), dist, reddening)
 period = data.Per.to_numpy()
 mass = mist_mass_interpolate(df, features, abs_mags, mag_str)
 # mass = data.Mass.to_numpy()
+tau = mist_tau_interpolate(features_with_tau, mass, age, age_err)
 
 fig, ax = plt.subplots(1, figsize=(11.5, 7))
 ax.invert_xaxis()
 ax.set(title=name, xlabel="Mass (M_Solar)", ylabel="Period (days)")
 ax.scatter(mass, period, color="green")
 
-data_dict = {"Per": period, "Mass": mass}
-data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass"])
+data_dict = {"Per": period, "Mass": mass, "Tau": tau}
+data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass", "Tau"])
 current_dict = {"age": age, "data_frame": data_frame}
 cluster_dict.update({name: current_dict})
 #%% NGC2547
@@ -423,14 +463,15 @@ abs_mags = app_to_abs(data.Vmag.to_numpy(), dist, reddening)
 
 period = data.Per.to_numpy()
 mass = mist_mass_interpolate(df, features, abs_mags, mag_str)
+tau = mist_tau_interpolate(features_with_tau, mass, age, age_err)
 
 fig, ax = plt.subplots(1, figsize=(11.5, 7))
 ax.invert_xaxis()
 ax.set(title=name, xlabel="Mass (M_Solar)", ylabel="Period (days)")
 ax.scatter(mass, period, color="green")
 
-data_dict = {"Per": period, "Mass": mass}
-data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass"])
+data_dict = {"Per": period, "Mass": mass, "Tau": tau}
+data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass", "Tau"])
 current_dict = {"age": age, "data_frame": data_frame}
 cluster_dict.update({name: current_dict})
 
@@ -460,14 +501,15 @@ abs_mags = app_to_abs(data.Gmag.to_numpy(), dist, reddening)
 period = data.Per.to_numpy()
 mass = mist_mass_interpolate(df, features, abs_mags, mag_str)
 # mass = data.Mass.to_numpy()
+tau = mist_tau_interpolate(features_with_tau, mass, age, age_err)
 
 fig, ax = plt.subplots(1, figsize=(11.5, 7))
 ax.invert_xaxis()
 ax.set(title=name, xlabel="Mass (M_Solar)", ylabel="Period (days)")
 ax.scatter(mass, period, color="green")
 
-data_dict = {"Per": period, "Mass": mass}
-data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass"])
+data_dict = {"Per": period, "Mass": mass, "Tau": tau}
+data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass", "Tau"])
 current_dict = {"age": age, "data_frame": data_frame}
 cluster_dict.update({name: current_dict})
 
@@ -496,14 +538,15 @@ abs_mags = app_to_abs(data.Ksmag.to_numpy(), dist, reddening)
 
 period = data.Prot.to_numpy()
 mass = mist_mass_interpolate(df, features, abs_mags, mag_str)
+tau = mist_tau_interpolate(features_with_tau, mass, age, age_err)
 
 fig, ax = plt.subplots(1, figsize=(11.5, 7))
 ax.invert_xaxis()
 ax.set(title=name, xlabel="Mass (M_Solar)", ylabel="Period (days)")
 ax.scatter(mass, period, color="green")
 
-data_dict = {"Per": period, "Mass": mass}
-data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass"])
+data_dict = {"Per": period, "Mass": mass, "Tau": tau}
+data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass", "Tau"])
 current_dict = {"age": age, "data_frame": data_frame}
 cluster_dict.update({name: current_dict})
 
@@ -535,7 +578,8 @@ print(
 )
 abs_mags = app_to_abs(data.Ksmag.to_numpy(), dist, reddening)
 
-mass = mist_mass_interpolate(df, features, abs_mags, "2MASS_Ks")
+mass = mist_mass_interpolate(df, features, abs_mags, mag_str)
+tau = mist_tau_interpolate(features_with_tau, mass, age, age_err)
 
 fig, ax = plt.subplots(1, figsize=(11.5, 7))
 ax.invert_xaxis()
@@ -577,23 +621,24 @@ abs_mags = app_to_abs(data.Ksmag.to_numpy(), dist, reddening)
 
 period = data.Per.to_numpy()
 mass = mist_mass_interpolate(df, features, abs_mags, mag_str)
+tau = mist_tau_interpolate(features_with_tau, mass, age, age_err)
 
 fig, ax = plt.subplots(1, figsize=(11.5, 7))
 ax.invert_xaxis()
 ax.set(title=name, xlabel="Mass (M_Solar)", ylabel="Period (days)")
 ax.scatter(mass, period, color="green")
 
-data_dict = {"Per": period, "Mass": mass}
-data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass"])
+data_dict = {"Per": period, "Mass": mass, "Tau": tau}
+data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass", "Tau"])
 current_dict = {"age": age, "data_frame": data_frame}
 cluster_dict.update({name: current_dict})
 
 #%% NGC6819
 name = "ngc6819"
 age = 2500e6
-age_err = 600*10**6
-dist = 2208 
-reddening = 0.006 #  0.41 to 0.89 mag
+age_err = 600 * 10 ** 6
+dist = 2208
+reddening = 0.006  #  0.41 to 0.89 mag
 path = "D:/dev/spin_down/new_data/" + "ngc6819/meibom_2015.tsv"
 
 
@@ -613,27 +658,29 @@ abs_mags = app_to_abs(data.V.to_numpy(), dist, reddening)
 
 period = data.PMean.to_numpy()
 mass = mist_mass_interpolate(df, features, abs_mags, mag_str)
+tau = mist_tau_interpolate(features_with_tau, mass, age, age_err)
 
 fig, ax = plt.subplots(1, figsize=(11.5, 7))
 ax.invert_xaxis()
 ax.set(title=name, xlabel="Mass (M_Solar)", ylabel="Period (days)")
 ax.scatter(mass, period, color="green")
 
-data_dict = {"Per": period,  "Mass": mass}
-data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass"])
+data_dict = {"Per": period, "Mass": mass, "Tau": tau}
+data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass", "Tau"])
 current_dict = {"age": age, "data_frame": data_frame}
 cluster_dict.update({name: current_dict})
 
-#%% m67 
+
+
+#%% m67
 name = "m67"
 age = 4200e6
-age_err = 1000*10**6
+age_err = 1000 * 10 ** 6
 dist = 816
-reddening = 0.047 #taylor 2007
+reddening = 0.047  # taylor 2007
 path1 = "D:/dev/spin_down/new_data/" + "m67/barnes_2016.csv"
 path2 = "D:/dev/spin_down/new_data/" + "m67/gonzalez_2016.tsv"
 path3 = "D:/dev/spin_down/new_data/" + "m67/gonzalez2_2016.tsv"
-
 
 
 data = pd.read_csv(path1, comment="#", delimiter="\t", skipinitialspace=True)
@@ -675,11 +722,12 @@ ax.scatter(mass2, period2, color="pink")
 ax.set(title=name, xlabel="Mass (M_Solar)", ylabel="Period (days)")
 ax.scatter(mass3, period3, color="orange")
 
-period_full = np.concatenate((period,period2,period3))
-mass_full = np.concatenate((mass,mass2,mass3))
+period_full = np.concatenate((period, period2, period3))
+mass_full = np.concatenate((mass, mass2, mass3))
+tau = mist_tau_interpolate(features_with_tau, mass_full, age, age_err)
 
-data_dict = {"Per": period_full,  "Mass": mass_full}
-data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass"])
+data_dict = {"Per": period_full, "Mass": mass_full, "Tau": tau}
+data_frame = pd.DataFrame(data_dict, columns=["Per", "Mass", "Tau"])
 current_dict = {"age": age, "data_frame": data_frame}
 cluster_dict.update({name: current_dict})
 #%% Plot
@@ -729,7 +777,7 @@ ax.scatter(
     cluster_dict["praesepe"]["data_frame"].Mass,
     cluster_dict["praesepe"]["data_frame"].Per,
     color="green",
-    label=cluster_dict["praesepe"]["age"]/ 1e6,
+    label=cluster_dict["praesepe"]["age"] / 1e6,
     marker="x",
     s=2,
 )
@@ -737,7 +785,7 @@ ax.scatter(
     cluster_dict["ngc6811"]["data_frame"].Mass,
     cluster_dict["ngc6811"]["data_frame"].Per,
     color="blue",
-    label=cluster_dict["ngc6811"]["age"]/ 1e6,
+    label=cluster_dict["ngc6811"]["age"] / 1e6,
     marker="x",
     s=2,
 )
@@ -745,16 +793,55 @@ ax.scatter(
     cluster_dict["ngc6819"]["data_frame"].Mass,
     cluster_dict["ngc6819"]["data_frame"].Per,
     color="red",
-    label=cluster_dict["ngc6819"]["age"]/ 1e6,
+    label=cluster_dict["ngc6819"]["age"] / 1e6,
     marker="x",
     s=2,
 )
 ax.legend()
 #%% Create CSV Files
 for name in cluster_list:
-    path = "D:/dev/spin_down/mistmade_data/{}_pm_{:e}.csv".format(name, cluster_dict[name]["age"]/ 1e6)
+    path = "D:/dev/spin_down/mistmade_data/{}_pm_{:e}.csv".format(
+        name, cluster_dict[name]["age"] / 1e6
+    )
     cluster_dict[name]["data_frame"].to_csv(path_or_buf=path, index=False, sep="\t")
 
+
+#%% TEST CELL
+
+fig, ax = plt.subplots(1, figsize=(8, 4.5))
+ax.set(xlim=(0, 0.5e11))
+ax.plot(features_with_tau.star_age, features_with_tau.conv_env_turnover_time_g)
+
+
+
+
+#%% TEST CELL #2
+rossby_ = [
+    [
+        [cluster_dict[name]["age"], item[1][0], item[1][1]]
+        for item in cluster_dict[name]["data_frame"].iterrows()
+    ]
+    for name in cluster_dict
+]
+
+flatten = lambda l: [item for sublist in l for item in sublist]
+rossby = pd.DataFrame(flatten(rossby_), columns=["age", "Per", "Tau"])
+
+fig, ax = plt.subplots(1)
+# ax.set(xlim = (0,10), ylim = (-0.2,20))
+ax.scatter(rossby.Per, rossby.Per / rossby.Tau)
+
+
+df = rossby
+labels, bins = pd.cut(df.Tau, 100, labels=False, retbins=True)
+
+unique_labels = np.unique(labels)
+
+binned_df = [df[labels == label] for label in unique_labels]
+
+
+fig, ax = plt.subplots(1)
+ax.scatter(binned_df[0].age, binned_df[0].Per)
 
 #%% TEMPLATE
 """
